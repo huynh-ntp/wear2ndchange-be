e0c0c6453f3fe6d3ff887927cc309d0c4865fa49
Trong Nhan <nhan.quang@xomad.com>
2025-05-26 13:02:03 +0700

    open cors

---
+++ src/main/java/com/huynhntp/commons/wear2ndchange/config/security/WebConfig.java
@@ -0,0 +1,22 @@
+package com.huynhntp.commons.wear2ndchange.config.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
